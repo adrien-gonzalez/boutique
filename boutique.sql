db0905d64a832c2d18f1ec50d1402cf83f79406f
adrien-gonzalez <adrien.gonzalez@laplateforme.io>
2020-02-26 14:55:22 +0100

    BDD

--- boutique.sql
+++ boutique.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 26 fév. 2020 à 13:33
+-- Généré le :  mer. 26 fév. 2020 à 13:55
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -121,6 +121,9 @@ DROP TABLE IF EXISTS `utilisateurs`;
 CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
