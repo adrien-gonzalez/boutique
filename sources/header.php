bd4b8894e42880458e15a835aa0d9443664b8c88
Thomas SAUSSOL <thomas.saussol@orange.fr>
2020-02-27 14:02:13 +0100

    connecté pas connecté fait

---
+++ sources/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<header>
+  <nav>
+        <ul>
+
+
+            <?php
+                if(isset($_SESSION['login']))
+              {/*conecté*/?>
+                  <li><a href="index.php">Accueil</a></li>
+                  <li><a href="profil.php">Profil</a></li>
+                  <li><a href="panier.php">Panier</a></li>
+                  <li><a href="deconnexion.php">Déconnexion</a></li>
+              <?php
+              }
+              else
+              { /*pas connecter */ ?>
+                  <li><a href="inscription.php">Inscription</a></li>
+                  <li><a href="connexion.php">Connexion</a></li>
+              <?php
+              }
+              ?>
+        </ul>
+  </nav>
+</header>
+
+</html>
