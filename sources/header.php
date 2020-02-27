e85e356987acfe33f1965879dc53ac39d26229fd
Thomas SAUSSOL <thomas.saussol@orange.fr>
2020-02-27 16:47:44 +0100

    header finit en partie reste a faire les catégorie et sous catégories

--- sources/header.php
+++ sources/header.php
@@ -13,14 +13,16 @@
                 if(isset($_SESSION['login']))
               {/*conecté*/?>
                   <li><a href="index.php">Accueil</a></li>
-                  <li><a href="profil.php">Profil</a></li>
-                  <li><a href="panier.php">Panier</a></li>
+                  <li><a href="profil.php">Catégories</a></li>
+                  <a href="profil.php"><img src="../img/logo_profil.png" height="40px" ></a>
+                  <a href="panier.php"><img src="../img/logo_panier.png" height="40px" ></a>
                   <li><a href="deconnexion.php">Déconnexion</a></li>
               <?php
               }
               else
               { /*pas connecter */ ?>
                   <li><a href="inscription.php">Inscription</a></li>
+                  <li><a href="profil.php">Catégories</a></li>
                   <li><a href="connexion.php">Connexion</a></li>
               <?php
               }
